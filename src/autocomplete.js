893e9188dc4c829976de54cea1b7a5ef1a74afee
yeffry <yeffry45@gmail.com>
2020-06-29 11:38:12 -0400

    autocomplete added dynamically through JS

---
+++ src/autocomplete.js
@@ -0,0 +1,17 @@
+const autoComplete = () => {
+	return `
+  <div class="autocomplete">
+    <label for="search">Search</label>
+    <input type="text" id="search" name="search">
+      <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content">
+    
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+export { autoComplete };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,47 @@
 // Project layout - Basic Idea
 // 1- User types a movie and a list of movies matching show up
 // 2- When the user clicks the give movie, stats about that movie show
+import {autoComplete} from './autocomplete'
 
+document.querySelector('.autocomplete-section').innerHTML = autoComplete();
 const key = '6d5ddc8d';
 
 const onMovieSearch = async (searchTerm) => {
-  const response = await fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`)
-    .then((data) => {
-      return data.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      alert(err);
+	const response = await fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`)
+		.then((data) => {
+			return data.json();
+		})
+		.then((data) => {
+			return data;
+		})
+		.catch((err) => {
+			alert(err);
     });
-
-  if (response.Error) {
-    return [];
+    
+  console.log(response)
+	if (response.Error) {
+		return [];
   }
-  return response.Search;
+  
+	return response.Search;
 };
-
-
-const dropdown = document.querySelector('.dropdown');
 
 const onInput = async (e) => {
-  const items = await onMovieSearch(e.target.value).then((data) => {
-    return data
+	const items = await onMovieSearch(e.target.value).then((data) => {
+		return data;
   });
-  console.log(items)
 
-  if (items.length) {
-    dropdown.style.display = 'block';
-  } else {
-    dropdown.style.display = 'none';
+  const dropdown = document.querySelector('.dropdown');
+	dropdown.innerHTML = '';
 
-  }
-
-  document.querySelector('.dropdown-content').innerHTML = '';
-
-  items.forEach((item) => {
-    const option = document.createElement('a');
-    option.textContent = item.Title;
-    option.classList.add('dropdown-item');
-    document.querySelector('.dropdown-content').appendChild(option);
-  });
+	items.forEach((item) => {
+		dropdown.innerHTML += `
+    <a href="" class="dropdown-item">
+      <img src=${item.Poster}>
+      ${item.Title}
+    </a>
+    `;
+	});
 };
+
 document.querySelector('#search').addEventListener('input', onInput);
