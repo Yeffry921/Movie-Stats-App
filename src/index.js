fb0bc28907a40570c200b782006fdbb7e48c5aef
yeffry <yeffry45@gmail.com>
2020-07-02 14:53:43 -0400

    Modify code and add Eslint dev devependecy

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import { autoComplete } from "./autocomplete";
-import "./style.css";
+import { autoComplete } from './autocomplete';
+import './style.css';
 
 // MOVIE CONTROLLER
 const movieController = (() => {
-  const key = "6d5ddc8d";
+  const key = '6d5ddc8d';
   const onMovieSearch = async (searchTerm) => {
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`
+      `http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`,
     )
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        return data;
-      })
+      .then((data) => data.json())
+      .then((data) => data)
       .catch((err) => {
+        // eslint-disable-next-line no-console
         console.log(err);
       });
     if (response.Error) {
@@ -25,15 +22,12 @@ const movieController = (() => {
 
   const onMovieSelect = async (movieName) => {
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${key}&t=${movieName}`
+      `http://www.omdbapi.com/?apikey=${key}&t=${movieName}`,
     )
-      .then((data) => {
-        return data.json();
-      })
-      .then((movie) => {
-        return movie;
-      })
+      .then((data) => data.json())
+      .then((movie) => movie)
       .catch((err) => {
+        // eslint-disable-next-line no-console
         console.log(err);
       });
 
@@ -49,16 +43,15 @@ const movieController = (() => {
 // UI CONTROLLLER
 const UIController = (() => {
   const DOMStrings = {
-    dropdown: ".dropdown",
-    results: ".results",
-    search: "#search",
-    dropdownContent: ".dropdown-content",
-    autocompleteSection: ".autocomplete-section",
-    movieStatSection: ".movie-stat-section",
+    dropdown: '.dropdown',
+    results: '.results',
+    search: '#search',
+    dropdownContent: '.dropdown-content',
+    autocompleteSection: '.autocomplete-section',
+    movieStatSection: '.movie-stat-section',
   };
 
-  const renderMovieStat = (data) => {
-    return `
+  const renderMovieStat = (data) => `
     <img src=${data.Poster}>
     <div>
       <h3 class="movie-data">Box Office: ${data.BoxOffice}</h3>
@@ -68,13 +61,12 @@ const UIController = (() => {
       <h3 class="movie-data">Awards: ${data.Awards}</h3>
     </div>
     `;
-  };
 
   const renderDropdownMovies = (movies) => {
     movies.forEach((item) => {
       let imgPoster = item.Poster;
-      if (imgPoster === "N/A") {
-        imgPoster = "";
+      if (imgPoster === 'N/A') {
+        imgPoster = '';
       }
       document.querySelector(DOMStrings.results).innerHTML += `
       <a href="#" class="dropdown-item">
@@ -94,66 +86,66 @@ const UIController = (() => {
 
 // GLOBAL CONTROLLER
 const globalAppController = ((movieCtrl, UICtrl) => {
+  // eslint-disable-next-line prefer-destructuring
   const DOMStrings = UICtrl.DOMStrings;
-
-  const init = () => {
-    document.querySelector(
-      DOMStrings.autocompleteSection
-    ).innerHTML = autoComplete();
-    loadEventListeners();
-  };
-
-  const loadEventListeners = () => {
-    document
-      .querySelector(DOMStrings.search)
-      .addEventListener("input", onInput);
-    document
-      .querySelector(DOMStrings.dropdownContent)
-      .addEventListener("click", getMovieStat);
-  };
-
-  const onInput = async (e) => {
-    // Fetch Movie List
-    const items = await movieCtrl.onMovieSearch(e.target.value).then((data) => {
-      return data;
-    });
-
-    const dropdown = document.querySelector(DOMStrings.dropdown);
-    const results = document.querySelector(DOMStrings.results);
-    // If there are no movies hide the dropdown and leave
-    if (!items.length) {
-      dropdown.style.display = "none";
-      // get out of function or the next line runs
-      return;
-    }
-    dropdown.style.display = "block";
-
-    results.innerHTML = "";
-
-    UICtrl.renderDropdownMovies(items);
-  };
-
   const clearAndHideDropdown = () => {
     const dropdown = document.querySelector(DOMStrings.dropdown);
 
-    document.querySelector(DOMStrings.search).value = "";
-    dropdown.style.display = "none";
+    document.querySelector(DOMStrings.search).value = '';
+    dropdown.style.display = 'none';
   };
+
   const clearAndRenderMovies = (movieData) => {
     const movieStatSection = document.querySelector(
-      DOMStrings.movieStatSection
+      DOMStrings.movieStatSection,
     );
 
-    movieStatSection.innerHTML = "";
+    movieStatSection.innerHTML = '';
     movieStatSection.innerHTML += UICtrl.renderMovieStat(movieData);
   };
   const getMovieStat = async (e) => {
-    if (e.target.className === "dropdown-item") {
+    if (e.target.className === 'dropdown-item') {
       const movieData = await movieCtrl.onMovieSelect(e.target.textContent);
       clearAndHideDropdown();
       clearAndRenderMovies(movieData);
     }
   };
+
+  const onInput = async (e) => {
+    // Fetch Movie List
+    const items = await movieCtrl.onMovieSearch(e.target.value).then((data) => data);
+
+    const dropdown = document.querySelector(DOMStrings.dropdown);
+    const results = document.querySelector(DOMStrings.results);
+    // If there are no movies hide the dropdown and leave
+    if (!items.length) {
+      dropdown.style.display = 'none';
+      // get out of function or the next line runs
+      return;
+    }
+    dropdown.style.display = 'block';
+
+    results.innerHTML = '';
+
+    UICtrl.renderDropdownMovies(items);
+  };
+
+  const loadEventListeners = () => {
+    document
+      .querySelector(DOMStrings.search)
+      .addEventListener('input', onInput);
+    document
+      .querySelector(DOMStrings.dropdownContent)
+      .addEventListener('click', getMovieStat);
+  };
+
+  const init = () => {
+    document.querySelector(
+      DOMStrings.autocompleteSection,
+    ).innerHTML = autoComplete();
+    loadEventListeners();
+  };
+
   return {
     init,
   };
